9fc19cbcaeaea78c1cec666b537951935465cf0b
Hashir Mehmood <125576695+hash1000@users.noreply.github.com>
2024-04-09 13:40:39 +0500

    navbar update and mongodb update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
-import { Outlet } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
-import Header from './components/Header';
-import './App.css';
-import currentYear from './utilities/currentYear';
+import { Outlet } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+// import currentYear from "./utilities/currentYear";
 
 const App = () => {
   return (
-    <div class="root-container">
-    <Header/>
-    <Outlet />
-    {/* <footer className='footer'> Copyright ©{currentYear()}. All rights reserved.</footer> */}
-  </div>
+    <>
+      <ToastContainer />
+      <div class="root-container">
+        <Header />
+        <Outlet />
+        {/* <footer className='footer'> Copyright ©{currentYear()}. All rights reserved.</footer> */}
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
-import { LinkContainer } from 'react-router-bootstrap';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { useLogoutMutation } from '../slices/usersApiSlice';
-import { logout } from '../slices/authSlice';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
+import { ChevronDown } from "react-bootstrap-icons";
 
 const Header = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -18,44 +19,55 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
-      navigate('/login');
+      navigate("/login");
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <header className='header'>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+    <header className="header">
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
+          <LinkContainer to="/">
             <Navbar.Brand>Definance Passive Empire</Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            {userInfo && userInfo.name}
+            <ChevronDown size={15} className="ms-1" />
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
               {userInfo ? (
                 <>
-                  <NavDropdown title={userInfo.name} id='username' style={{textAlign:"end"}}>
-                    <LinkContainer to='/profile'>
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to='/subscription'>
-                      <NavDropdown.Item>Subscriptions</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                  <Navbar.Text
+                    className="d-none d-lg-block"
+                    style={{
+                      marginRight: "15px",
+                      fontSize: "1.5rem",
+                      fontWeight: "bold",
+                      padding: "0px",
+                    }}
+                  >
+                    {userInfo.name}
+                  </Navbar.Text>
+
+                  <LinkContainer to="/profile">
+                    <Nav.Link>Profile</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/subscription">
+                    <Nav.Link>Subscriptions</Nav.Link>
+                  </LinkContainer>
+                  <Navbar.Text style={{ cursor: "pointer" }} onClick={logoutHandler}>Logout</Navbar.Text>
                 </>
               ) : (
                 <>
-                  <LinkContainer to='/login'>
+                  <LinkContainer to="/login">
                     <Nav.Link>
                       <FaSignInAlt /> Sign In
                     </Nav.Link>
                   </LinkContainer>
-                  <LinkContainer to='/register'>
+                  <LinkContainer to="/register">
                     <Nav.Link>
                       <FaSignOutAlt /> Sign Up
                     </Nav.Link>
