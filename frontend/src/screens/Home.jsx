17fc9ecd25a09c5f263a1c166179f43d9c995f10
Hashir Mehmood <125576695+hash1000@users.noreply.github.com>
2024-04-16 02:18:30 +0500

    escape functionality added

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import "../App.css";
 
@@ -9,35 +9,55 @@ function Home() {
     frameworkUrl: "/game-build/Definance_WebGL_Build.framework.js",
     codeUrl: "/game-build/Definance_WebGL_Build.wasm",
   });
-  function toggleFullScreen() {
-    var header = document.getElementsByClassName('header')[0];
-    var container=document.getElementsByClassName('root-container')[0];
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-      header.classList.add('hidden');
-      container.classList.add('full-container');
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-        header.classList.remove('hidden');
-        container.classList.remove('full-container');
-      } 
-    }
-  }
+  useEffect(() => {
+    const unityContainer = document.querySelector(".unity-container");
+    const unityFooter = document.querySelector(".unity-footer");
+
+    const header = document.querySelector(".header");
+    const container = document.querySelector(".root-container");
+    const toggleFullScreen = () => {
+
+      console.log("i am here in toggleFullScreen");
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+        header.classList.add("hidden");
+        container.classList.add("full-container");
+        unityContainer.classList.add("unity-container-full");
+        unityFooter.classList.add("hidden");
+      } else  {
+        if(document.exitFullscreen){
+          document.exitFullscreen();
+          header.classList.remove("hidden");
+          container.classList.remove("full-container");
+          unityContainer.classList.remove("unity-container-full");
+          unityFooter.classList.remove("hidden");
+        }
+      }
+    };
+
+    unityFooter.addEventListener("click", toggleFullScreen);
+    document.addEventListener("keydown", (e)=>{
+      if (e.key === "Escape") {
+          header.classList.remove("hidden");
+          container.classList.remove("full-container");
+          unityContainer.classList.remove("unity-container-full");
+          unityFooter.classList.remove("hidden");
+      }
+     })
+    return () => {
+      unityFooter.removeEventListener("click", toggleFullScreen);
+      document.removeEventListener("keydown");
+    };
+  }, []);
 
   return (
     <div style={{ position: "relative" }}>
       <div className="unity-container">
         {isLoaded === false && (
           <div className="loading">
-            <div className="loadingText">
-                {Math.round(loadingProgression * 100)}%
-              </div>
+            <div className="loadingText">{Math.round(loadingProgression * 100)}%</div>
             <div className="loadingBar">
-              <div
-                className="loadingBarFill"
-                style={{ width: `${loadingProgression * 100}%` }}
-              />
+              <div className="loadingBarFill" style={{ width: `${loadingProgression * 100}%` }} />
             </div>
           </div>
         )}
@@ -59,8 +79,6 @@ function Home() {
             style={{ marginLeft: "10px", marginTop: "1px" }}
             src="/fullscreen-button.png"
             alt="Fullscreen"
-            id="unity-fullscreen-button"
-            onClick={toggleFullScreen}
           />
         </div>
       </div>
